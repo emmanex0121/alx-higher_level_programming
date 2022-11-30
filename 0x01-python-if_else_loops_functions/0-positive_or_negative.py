0548d2c78fa743c8f9f3325bb76b67e72eeed60b
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-11-30 14:14:42 -0500

    Determine if a random generated number is +ve or _ve

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(f"{number:d} is positive", end='')
+    print(f"{number:d} is positive")
 elif number == 0:
-    print(f"{number:d} is zero", end='')
+    print(f"{number:d} is zero")
 else:
-    print(f"{number:d} is negative", end='')
+    print(f"{number:d} is negative")
