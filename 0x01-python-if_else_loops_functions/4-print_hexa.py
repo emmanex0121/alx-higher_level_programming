2cb9db887037e0f9bc8b0c59e8b2eeb88df92afa
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-11-30 17:22:36 -0500

    Print 0 to 98 in int and hex

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(0, 99):
-    print("{:d} = {:#x}".format(i, i))
-    #print("{:d} = {}".format(i, hex(i)))
+    #print("{:d} = {:#x}".format(i, i))
+    print("{:d} = {}".format(i, hex(i)))
