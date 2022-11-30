05e4471f00a75d1cf5b5e7404f0dfbb307fa1474
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-11-29 19:23:16 -0500

    C program to check loop

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+	slow = fast = list;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			printf(
