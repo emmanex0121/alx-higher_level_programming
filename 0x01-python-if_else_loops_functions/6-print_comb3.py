92160544ef30e6615a3462d357af1f32d5075fa1
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-12-01 06:35:56 -0500

    Prints possible combination of two numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 
 for digit1 in range(0, 10):
     for digit2 in range(digit1 + 1, 10):
-        if digit1 is 8 and digit2 is 9:
+        if digit1 == 8 and digit2 == 9:
             print("{:d}{:d}".format(digit1, digit2))
         else:
             print("{:d}{:d}".format(digit1, digit2), end=', ')
