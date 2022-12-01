c9210d1b2a2605871b22297cb16d88b9207fa9b3
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-12-01 07:08:03 -0500

    Function that Checks for the case of a character

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,12 @@
 #!/usr/bin/pyhton3
 
-c = input("enter a single ascii char: ")
+#c = input("enter a single ascii char: ")
+
 def islower(c):
     for i in range(97, 123):
         if chr(i) == c:
             return True
-        else:
-            return False
 
-print("{} is {}".format(c, "lower" if islower(c) else "upper"))
+        #else:
+            #return False
+#print("{} is {}".format(c, "lower" if islower(c) else "upper"))
