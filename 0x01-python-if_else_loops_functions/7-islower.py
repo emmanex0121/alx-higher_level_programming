a84a431ffb3cebc6da06c4936e11dd6169b1c217
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-12-01 07:27:54 -0500

    Function that Checks for the case of a character

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -6,10 +6,3 @@ def islower(c):
     else:
         return False
 
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
-print("7 => {}".format("lower" if islower(7) else "upper"))
-print("'' => {}".format("lower" if islower("") else "upper"))
