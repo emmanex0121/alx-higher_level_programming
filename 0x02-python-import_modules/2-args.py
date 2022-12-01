5deff2f2d0d35812af320c18717dfa20907b639f
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-12-01 14:59:21 -0500

    Counts arguments passed to stdin and prints

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
 
 if len(argv) == 2:
     print("{:d} argument:".format(len(argv) - 1))
+elif len(argv) == 1:
+    print("{:d} arguments.".format(len(argv) - 1))
 else:
     print("{:d} arguments:".format(len(argv) - 1))
 
