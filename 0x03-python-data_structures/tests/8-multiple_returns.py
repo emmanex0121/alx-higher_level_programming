b2deb9ccf80dfc8c2877981216e74d0c9d8a2cd5
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-12-04 15:05:29 -0500

    Function that takes in string and returns a tuple

--- 0x03-python-data_structures/tests/8-multiple_returns.py
+++ 0x03-python-data_structures/tests/8-multiple_returns.py
@@ -3,7 +3,10 @@
 def multiple_returns(sentence):
     str_len = len(sentence)
     str_first = sentence[0]
-    return (str_len, str_first)
+    if str_len = 0:
+        return (str_len, None)
+    else:
+        return (str_len, str_first)
 
     '''
             OR DO THIS
