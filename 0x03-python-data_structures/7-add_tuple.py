d1a6eb9a3c12b163056b867f50296a6a7ccab982
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-12-04 14:48:59 -0500

    Function that adds the elements of a tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,7 +7,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_a = tuple_a[0], 0
         else:
             tuple_a = 0, 0
-            
+
     if len(tuple_b) < 2:
         if len(tuple_b) == 0:
             tuple_b = 0, 0
