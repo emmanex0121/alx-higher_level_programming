c5dd05172c0cade993c984456e015336d972c5cc
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-12-04 19:21:17 -0500

    hot - fix for task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    new = []
     for elem in matrix:
         for num in elem:
             if num == elem[-1]:
-                print('{:d}'.format(num))
+                print('{:d}'.format(num), end='')
             else:
                 print('{:d}'.format(num), end=' ')
+        print()
