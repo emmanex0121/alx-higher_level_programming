074bbb95a3016e3fd109ac95e6b1ffb60c8611c3
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-12-04 19:27:38 -0500

    hot - fix for task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,7 +4,7 @@ def print_reversed_list_integer(my_list=[]):
     if my_list:
         for num in reversed(my_list):
             print('{:d}'.format(num))
-    
+
     '''
         OR THIS WAY
     my_list.reverse()
