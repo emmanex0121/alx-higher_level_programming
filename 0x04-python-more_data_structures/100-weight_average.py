5ce6621853ddbba1e51ed8287b1ec0df09dc11fc
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-12-07 18:39:00 -0500

    hot-fix

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def weight_average(my_list=[]):
     if my_list and len(my_list):
         num = 0
--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def complex_delete(a_dictionary, value):
     keys_to_del = []
     for key in a_dictionary:
