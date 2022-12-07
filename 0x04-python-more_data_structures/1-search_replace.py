5b52c919a2432a7836e51ea6f344b159d154ab95
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-12-07 18:45:26 -0500

    Find and replace value in alist

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    for num in range(len(new_list)):
+        if new_list[num] == search:
+            new_list[num] = replace
+    return new_list
