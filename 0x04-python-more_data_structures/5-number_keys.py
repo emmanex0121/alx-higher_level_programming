719ac8676cbb161c0252bb993104b3d3960950a8
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-12-07 19:16:31 -0500

    Counts number of keys in a dictionary

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def number_keys(a_dictionary):
-    #return x.count(a_dictionary.keys())
-    return len(a_dictionary)
+    return len(a_dictionary.keys())
+    '''#return x.count(a_dictionary.keys())'''
