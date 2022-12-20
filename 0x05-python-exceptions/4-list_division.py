3199ace510691142cacd726b61e7b9b4f1573749
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-12-20 17:29:55 -0500

    Function that list elem 1 by list elem 2

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,5 +15,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             new_list.append(0)
+        finally:
+            pass
 
     return new_list
