6af798f6603cb9da0cfdf1e6d47f9ec473933465
Emmanuel Nwachukwu <emmax0121@gmail.com>
2022-12-20 17:31:41 -0500

    Alternate Function that list elem 1 by list elem 2

--- 0x05-python-exceptions/test/4-list_division.py
+++ 0x05-python-exceptions/test/4-list_division.py
@@ -5,17 +5,17 @@ def list_division(my_list_1, my_list_2, list_length):
 
     for i in range(list_length):
         try:
-            new_list.append(my_list_1[i] / my_list_2[i])
+            div_num =  my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
-            new_list.append(0)
+            div_num = 0
         except TypeError:
             print("wrong type")
-            new_list.append(0)
+            div_num = 0
         except IndexError:
             print("out of range")
-            new_list.append(0)
+            div_num = 0
         finally:
-            pass
+            new_list.append(div_num)
 
     return new_list
