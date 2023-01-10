380b9c1f6e6b1efa0a7788e9768c2a91a81e7012
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-01-10 14:04:06 -0500

    Class Raises Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module Defines a BaseGeometry class """
+
+
+class BaseGeometry:
+    """ EBase Geometry Class """
+    def area(self):
+        """ Raises and Exception """
+        raise Exception("area() is not implemented")
