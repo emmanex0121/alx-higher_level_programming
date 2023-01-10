5fe130fedeaa073c261cfb9593b6c9c41ed43323
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-01-10 09:40:38 -0500

    Returns attribute and objects of objects

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+
+This module returns list of availiable attributes
+and methods of an object 
+
+"""
+
+def lookup(obj):
+    """ This function finds and returns all the attributesand objects """
+    return dir(obj)
