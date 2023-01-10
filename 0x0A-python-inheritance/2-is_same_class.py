bf85981f0aac35ee657bdcbe9790d2a287661622
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-01-10 13:13:06 -0500

    Test if object exactly instance of class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Checks if object is an instance of a class """
 
+
 def is_same_class(obj, a_class):
     """ Returns True if object is exactly an instance of the specified class
         Returns False otherwise
