0beb4640ea9fce729b10972e45d6a3e21bb13711
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-01-10 16:35:11 -0500

    hot - fix

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,18 +1,34 @@
-import ctypes
+#include <stdio.h>
+#include <string.h>
+#include <Python.h>
 
-lib = ctypes.CDLL('./libPython.so')
-lib.print_python_string.argtypes = [ctypes.py_object]
-s = "The spoon does not exist"
-lib.print_python_string(s)
-s = "ложка не существует"
-lib.print_python_string(s)
-s = "La cuillère n'existe pas"
-lib.print_python_string(s)
-s = "勺子不存在"
-lib.print_python_string(s)
-s = "숟가락은 존재하지 않는다."
-lib.print_python_string(s)
-s = "スプーンは存在しない"
-lib.print_python_string(s)
-s = b"The spoon does not exist"
-lib.print_python_string(s)
+/**
+ * print_python_string - Prints string information
+ *
+ * @p: Python Object
+ * Return: no return
+ */
+void print_python_string(PyObject *p)
+{
+
+	PyObject *str, *repr;
+
+	(void)repr;
+	printf("[.] string object info\n");
+
+	if (strcmp(p->ob_type->tp_name, "str"))
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+
+	repr = PyObject_Repr(p);
+	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
+	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
+	printf("  value: %s\n", PyBytes_AsString(str));
+}
