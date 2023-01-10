8974cb6122bc0ced3affc8c3eda19a068492cee9
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-01-10 15:59:02 -0500

    Square class inherits Rectangle and gets Area

--- 0x0A-python-inheritance/test/11-square.py
+++ 0x0A-python-inheritance/test/11-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module Defines class Square that Inherits from Rectangle"""
+"""Module Defines class Rectangle that Inherits from baseGeometry"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
@@ -7,5 +7,5 @@ class Square(Rectangle):
     """Defines the class Square"""
     def __init__(self, size):
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(size, size)
