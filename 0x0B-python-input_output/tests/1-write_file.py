b4529e5caabf6815ba58243aaa245675467b6d85
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-01-11 07:56:56 -0500

    Writes strings to file

--- 0x0B-python-input_output/tests/1-write_file.py
+++ 0x0B-python-input_output/tests/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Modeule defining writing to file"""
 
+
 def write_file(filename="", text=""):
     """Writes strings to files and return number of characters written"""
     with open(filename, "w", encoding="UTF8") as f:
