8fa6a8927155e015423c44f8fa2e86c1694c722d
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-01-11 07:27:37 -0500

    Reads file and prints to stdout

--- 0x0B-python-input_output/tests/0-read_file.py
+++ 0x0B-python-input_output/tests/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """ Reads a text file and prints to stdout """
     with open(filename, encoding="UTF8") as h:
         file_data = h.read()
-    print(file_data)
+    print(file_data, end="")
