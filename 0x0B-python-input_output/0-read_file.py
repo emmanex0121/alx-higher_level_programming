df1f4319a5ae9c77b8431fa5d08c2319453804ed
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-01-11 07:14:29 -0500

    Reads file and prints to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module defining reading and writing from file to stdout """
+
+
+def read_file(filename=""):
+    """ Reads a text file and prints to stdout """
+    with open(filename, encoding="UTF8") as h:
+        file_data = h.read()
+    print(file_data)
