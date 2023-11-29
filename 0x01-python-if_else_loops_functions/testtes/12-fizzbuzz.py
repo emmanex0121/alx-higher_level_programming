77f9dc5bc104d37f958d902294b758fa81459d9b
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-11-29 19:58:44 +0100

    FizzBuzz Numbers 1 to 100

--- 0x01-python-if_else_loops_functions/testtes/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/testtes/12-fizzbuzz.py
@@ -3,10 +3,10 @@
 
 for i in range(1, 101):
     if (i % 5 == 0) and (i % 3 == 0):
-        print(f"{'FizzBuzz' }", end='')
+        print(f"'FizzBuzz '", end='')
     elif i % 3 == 0:
-        print(f"{'Fizz' }", end='')
+        print(f"'Fizz '", end='')
     elif i % 5 == 0:
-        print(f"{'Buzz' }", end='')
+        print(f"'Buzz '}", end='')
     else:
-        print(f"{i}", end='')
+        print(f"{i} ", end='')
