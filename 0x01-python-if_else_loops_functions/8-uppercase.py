f256bae91d5b4ba19567f326e39c3a28f1bd745f
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-11-29 18:51:11 +0100

    Task 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#Program By Phoenix
+
+def uppercase(str):
+    for i in range(len(str)):
+        if str[i] in ' 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            num = 0
+        else:
+            num = 32
+        print("{:c}".format(ord(str[i]) - num), end='')
+    print()
