8909e4bb3d964ddd126342cbffd787144d0b7022
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-11-29 19:05:38 +0100

    Task 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase_method2.py
+++ 0x01-python-if_else_loops_functions/8-uppercase_method2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# Program by phoenix
+# Program By Phoenix
 
 def uppercase(str):
     for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            num = 32
-        else:
+        if str[i] in ' 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ':
             num = 0
+        else:
+            num = 32
         print("{:c}".format(ord(str[i]) - num), end='')
     print()
