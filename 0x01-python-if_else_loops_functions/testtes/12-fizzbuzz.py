e121e69a3a3ad3f1dda0aa72751ca01b1e26764d
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-11-29 20:02:23 +0100

    FizzBuzz Numbers 1 to 100

--- 0x01-python-if_else_loops_functions/testtes/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/testtes/12-fizzbuzz.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # Program By Phoenix
 
-for i in range(1, 101):
-    if (i % 5 == 0) and (i % 3 == 0):
-        print(f"'FizzBuzz '", end='')
-    elif i % 3 == 0:
-        print(f"'Fizz '", end='')
-    elif i % 5 == 0:
-        print(f"'Buzz '}", end='')
-    else:
-        print(f"{i} ", end='')
+def fizzbuzz():
+    for i in range(1, 101):
+        if (i % 5 == 0) and (i % 3 == 0):
+            print(f"FizzBuzz ", end='')
+        elif i % 3 == 0:
+            print(f"Fizz ", end='')
+        elif i % 5 == 0:
+            print(f"Buzz ", end='')
+        else:
+            print(f"{i} ", end='')
---
+++ 0x01-python-if_else_loops_functions/testtes/12-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# program by phoenix
+
+fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
+
+fizzbuzz()
+print("")
