3a302009240f31462fcca020907f6cd3b33c3b36
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-11-29 19:27:14 +0100

    Task 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,7 +4,7 @@
 def print_last_digit(number):
     if number < 0:
         last_num = -number % 10
-    else:
+    elif number >= 0:
         last_num = number % 10
     print("{}".format(last_num))
     return last_num
