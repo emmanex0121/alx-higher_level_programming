c5fd236544a75134dc22dbc61b1c91ed05732e85
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-11-30 18:57:55 +0100

    2-args.py Prints arguments

---
+++ 0x02-python-import_modules/2-args_method2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+
+if len(argv) == 2:
+    print("{:d} argument:".format(len(argv) - 1))
+elif len(argv) == 1:
+    print("{:d} arguments.".format(len(argv) - 1))
+else:
+    print("{:d} arguments:".format(len(argv) - 1))
+
+for i in range(1, len(argv)):
+    print("{:d}: {}".format(i, argv[i]))
