a2f5ec137a2dd98c4b9a0b851a0071cd04ceab64
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-11-30 19:14:17 +0100

    2-args.py Prints arguments

--- 0x02-python-import_modules/testes/2-args.py
+++ 0x02-python-import_modules/testes/2-args.py
@@ -3,8 +3,11 @@
 if __name__ == '__main__':
     from sys import argv
 
-if len(argv) < 1:
+if len(argv) < 2:
     print("0 arguments.")
+elif len(argv) == 2:
+    print(f"1 argument:")
+    print(f"1: {argv[1]}")
 else:
     print(f"{len(argv) - 1} arguments:")
     for i in range(1, len(argv)):
