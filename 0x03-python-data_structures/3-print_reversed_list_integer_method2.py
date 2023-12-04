1f51efcfa2a43ddc7eec0d9666e619f31a2f2e4d
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-12-05 00:33:41 +0100

    Function to reverse list

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer_method2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        for num in reversed(my_list):
+            print('{:d}'.format(num))
+
+    '''
+        OR THIS WAY
+    my_list.reverse()
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
+    '''
