06a31cc8f8f3c9214993b1df33b85e33a3b77f1a
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-12-05 01:23:26 +0100

    replace element at index of list copy

--- 0x03-python-data_structures/testes/4-new_in_list.py
+++ 0x03-python-data_structures/testes/4-new_in_list.py
@@ -5,6 +5,5 @@ def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     if idx < 0 or idx >= len(my_list):
         return new_list
-    for num in range(len(new_list)):
-        new_list[idx] = element
+    new_list[idx] = element
     return new_list
