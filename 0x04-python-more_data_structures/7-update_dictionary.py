3fee9a0120ca14a333be3001b1b1a6b533a3c016
Emmanuel Nwachukwu <emmax0121@gmail.com>
2023-12-07 04:28:18 +0100

    update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -5,6 +5,7 @@ def update_dictionary(a_dictionary, key, value):
     for k in a_dictionary:
         if k == key:
             a_dictionary[k] = value
-            break
+            return a_dictionary
     else:
         a_dictionary[key] = value
+        return a_dictionary
