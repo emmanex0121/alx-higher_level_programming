d07cd44f1f6affd4f93888912896340585b32667
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-01-07 00:50:38 +0100

    function print my name module

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# Program by Phoenix
+
+"""
+    Module say_my_name
+"""
+def say_my_name(first_name, last_name=""):
+    """Function that prints my name
+
+    Test Case first name is int:
+    >>> say_my_name(12, "phoenix")
+    Traceback (most recent call last):
+        ...
+    TypeError: first_name must be a string
+
+    Test case last name is int:
+    >>> say_my_name("kheed", 16)
+    Traceback (most recent call last):
+        ...
+    TypeError: last_name must be a string
+
+    >>> say_my_name("Phoenix", "Kheed")
+    My name is Phoenix Kheed
+    """
+    if type(first_name) != (str):
+        raise TypeError("first_name must be a string")
+    if type(last_name) != (str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
