9672ada02baec5bf058776541dcb45db4e4e79f4
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-01-10 00:11:57 +0100

    Final Changes

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,8 +8,8 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 a = []
 a = argv[1:]
-#for i in range(1, len(argv)):
-    #a.append(argv[i])
+# for i in range(1, len(argv)):
+# a.append(argv[i])
 
 try:
     a = load_from_json_file('add_item.json') + a
