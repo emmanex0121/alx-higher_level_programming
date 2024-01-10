214b8fade042f57e5a354c845f13b07bd772684e
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-01-10 16:27:18 +0100

    changes

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,41 +1,12 @@
 #!/usr/bin/python3
 # PROGRAM BY PHOENIX
-
 """
     module add_integer
 """
 
 
 def add_integer(a, b=98):
-    """Function That Adds the integer arguments.
-
-    example in list with integer values:
-    >>> [add_integer(n+2, n) for n in range(5)]
-    [2, 4, 6, 8, 10]
-
-    Negative int are allowed:
-    >>> add_integer(-3, 6)
-    3
-
-    floats are casted/converted to int:
-    >>> add_integer(3.9, 5)
-    8
-
-    >>> add_integer(7, 5.5)
-    12
-
-    Strings are not allowed and will raise a TypeError:
-    >>> add_integer(-3, "string")
-    Traceback (most recent call last):
-        ...
-    TypeError: b must be an integer
-
-    >>> add_integer("h", 14)
-    Traceback (most recent call last):
-        ...
-    TypeError: a must be an integer
-
-    """
+    """Function That Adds the integer arguments."""
 
     if type(a) not in (float, int):
         raise TypeError("a must be an integer")
