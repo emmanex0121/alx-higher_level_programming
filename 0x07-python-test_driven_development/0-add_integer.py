f85a0e6a810dd88b9ebad05212d379af1ec1cfa3
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-01-10 17:27:46 +0100

    commit: 4

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,6 +10,9 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in (float, int):
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
+    if type(a) is float:    
+    	a = int(a)
+    if type(b) is float:
+    	b = int(b)
+    	
     return a + b
