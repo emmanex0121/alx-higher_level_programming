6a9e294bb7c52bd0dd6c28862f72760de64d0cfd
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-01-10 16:56:38 +0100

    changes

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-# PROGRAM BY PHOENIX
-""" module add_integer """
+""" 0-add_integer Module """
 
 
 def add_integer(a, b=98):
-    """Function That Adds the integer arguments."""
+    """
+    Adds two integers
 
-    if type(a) not in (float, int):
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        addition of two integers
+    """
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) not in (float, int):
+    elif type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return a + b
+    else:
+        new_a, new_b = int(a), int(b)
+        return new_a + new_b
