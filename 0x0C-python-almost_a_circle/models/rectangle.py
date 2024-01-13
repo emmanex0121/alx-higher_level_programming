e3fcd96b3aaeca7d3adff9c5d112bccbe60e2805
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-01-13 17:52:50 +0100

    Task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,37 +3,87 @@
 """ module First Rectangle """
 from models.base import Base
 
+def run_checks(**kwargs):
+    """
+        Function creates valuecheck and Typecheck functions
+        And invokes them after
+    """
 
+    def type_check(**values):
+        """
+            Function that takes keyword arguments
+            Using Keword arguments dictionary style to seperate arg name
+            from its value
+            Raises a TypeError if value is not integer
+        """
+        for value in values:
+            if type(values[value]) is not int:
+                raise TypeError(f"{value} must be an integer")
+
+    def value_check(**values):
+        """
+            Function that takes keyword arguments
+            Using Keword arguments dictionary style to seperate arg name
+            from its value
+            Raises a ValueError if value <= 0 for width and height
+            Raises a ValueError if value < 0 for x and y
+        """
+        for value in values:
+            if value in ("width","height"):
+                if values[value] <= 0:
+                    raise ValueError(f"{value} must be > 0")
+            if value in ("x","y"):
+                if values[value] < 0:
+                    raise ValueError(f"{value} must be >= 0")
+
+    type_check(**kwargs)
+    value_check(**kwargs)
+
+    
 class Rectangle(Base):
     """ Rectangle class inheriting from base class """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
+        run_checks(width=width, height=height, x=x, y=y)
+
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
 
-    def set_width(self, width):
-        self.__width = width
-
-    def get_width(self):
+    @property
+    def width(self):
         return self.__width
 
-    def set_height(self, height):
-        self.__height = height
-
-    def get_height(self):
+    @width.setter
+    def width(self, width):
+        run_checks(width=width)
+        self.__width = width
+    
+    @property
+    def height(self):
         return self.__height
 
-    def set_x(self, x):
-        self.__x = x
+    @height.setter
+    def height(self, height):
+        run_checks(height=height)
+        self.__height = height
 
-    def get_x(self):
+    @property
+    def x(self):
         return self.__x
 
-    def set_y(self, y):
-        self.__y = y
+    @x.setter
+    def x(self, x):
+        run_checks(x=x)
+        self.__x = x
 
-    def get_y(self):
+    @property
+    def y(self):
         return self.__y
+
+    @y.setter
+    def y(self, y):
+        run_checks(y=y)
+        self.__y = y
