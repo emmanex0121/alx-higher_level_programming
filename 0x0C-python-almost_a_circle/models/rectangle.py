74bb677ea21e7ee34ca01fe6f9d32cf72303144c
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-01-13 16:36:34 +0100

    tast 0 1 and 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,23 +14,26 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
-
     def set_width(self, width):
         self.__width = width
+
     def get_width(self):
         return self.__width
 
     def set_height(self, height):
         self.__height = height
+
     def get_height(self):
         return self.__height
 
     def set_x(self, x):
         self.__x = x
+
     def get_x(self):
         return self.__x
 
     def set_y(self, y):
         self.__y = y
+
     def get_y(self):
         return self.__y
