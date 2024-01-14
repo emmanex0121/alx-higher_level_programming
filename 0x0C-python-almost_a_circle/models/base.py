8ae9eea7525f51d427c4b371bfb944ce8d5f59c4
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-01-14 04:06:40 +0100

    json string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # Program By Phoenix
 """ Module Base class """
+import json
 
 
 class Base:
@@ -19,8 +20,28 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        import json
-
+        """Return the JSON serialization of a list of dicts.
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
         if list_dictionaries in ([], None):
             return []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON serialization of a list of objects to a file.
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
+        filename = cls.__name__ + '.json'
+        with open(filename, 'w', encoding="utf-8") as json_file:
+            if list_objs is None:
+                json.dump([], json_file)
+            else:
+                ''' list_dicts = Base.to_json_string(list_objs)'''
+                list_dicts = []
+                for i in list_objs:
+                    list_dicts.append(i.to_dictionary())
+                json.dump(list_dicts, json_file)
+                '''json_file.write(Base.to_json_string(list_dicts))'''
