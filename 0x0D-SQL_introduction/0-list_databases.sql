0ed3aa0ecf5fa6e4c370b3bece84bd739917edd1
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-02-13 21:00:40 +0100

    Readme added, list all databses

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,4 +1,3 @@
--- 3 first students in the Batch ID=3
+-- MySql Script By Phoenix
 -- mysql query to list all databases on MySql server
--- SELECT id, name FROM students WHERE batch_id = 3 ORDER BY created_at DESC LIMIT 3;
 SHOW DATABASES;
