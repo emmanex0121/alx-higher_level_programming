c625874c93533067d1f4111444c0dcfe11baa0c7
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-02-14 02:41:57 +0100

    modded task 3

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,4 @@
 -- MySql Script By Phoenix
 -- Show tables in a database
-USE mysql; SHOW TABLES;
+-- USE mysql;
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- MySql Script By Phoenix
+-- Script that Creates a table in current MySql Server Database
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+	);
