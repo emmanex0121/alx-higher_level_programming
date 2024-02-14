bac09146b2e85cfc6c316f2bf3231e33c410e193
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-02-14 03:02:54 +0100

    modded task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,3 @@
 -- MySql Script By Phoenix
 -- Script that Creates a table in current MySql Server Database
-CREATE TABLE IF NOT EXISTS first_table (
-	id INT,
-	name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
