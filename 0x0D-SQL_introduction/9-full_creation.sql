ab9a162b2bea226654ec06c69648f5d3d11b593f
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-02-14 04:18:54 +0100

    create new table add id, name, score

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- MySql Script By Phoenix
+-- Create a second table and insert data
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'john', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
