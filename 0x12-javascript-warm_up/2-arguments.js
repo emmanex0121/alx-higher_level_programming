f53b586b43a02b11fa3a189ab38ed02a786be7c1
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-03-12 01:21:52 +0100

    Checking for arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const argLength = process.argv.length;
-if (argLength > 2) {
-  console.log('Arguments found');
+if (argLength === 3) {
+  console.log('Argument found');
+} else if (argLength > 3) {
+	console.log('Arguments found');
 } else {
   console.log('NO arguments');
 }
