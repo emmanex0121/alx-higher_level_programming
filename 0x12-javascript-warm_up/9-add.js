2ab63b6130b6dbbea819110f0d2d6a995e2ed206
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-03-12 04:05:34 +0100

    Addition script

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,18 +1,5 @@
 #!/usr/bin/node
 
-const arg = process.argv[2];
-const num1 = Number(arg);
-const num2 = Number(process.argv[3]);
-//const argLen = process.argv.length;
-
-/*
-if (arg === undefined || isNaN(num1) || isNaN(num2) || argLen === 3){
-	console.log('NaN');
-}else{
-	const sum = num1 + num2;
-	console.log(sum);
-}
-*/
-const sum = num1 + num2;
+const sum = process.argv[2] + process.argv[3];
 console.log(sum);
 
