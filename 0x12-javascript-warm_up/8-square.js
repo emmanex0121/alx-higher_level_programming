f69a24d6eb0d2e3814362ff51cfe9fed204a69aa
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-03-12 03:40:35 +0100

    Print square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+const num = Number(arg);
+
+if (arg === undefined){
+	console.log('Missing size');
+}
+
+for (let i = 0; i < num; i++){
+	let arr;
+	if (arr === undefined){
+		arr = 'X';
+		for (let j = 0; j < num - 1; j++){
+			arr += 'X';
+		}}
+	console.log(arr);
+}
