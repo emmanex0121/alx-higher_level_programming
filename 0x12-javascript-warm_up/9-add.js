b13998267a79bef95ca69abad820a2e722d391ce
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-03-12 04:10:10 +0100

    Addition script

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
-const sum = process.argv[2] + process.argv[3];
-console.log(sum);
-
+const a = process.argv[2];
+const b = process.argv[2];
+function add(a, b) {
+	const sum = a + b;
+	console.log(sum);
+}
+add();
