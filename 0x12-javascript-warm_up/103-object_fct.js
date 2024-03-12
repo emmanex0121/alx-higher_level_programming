e21b6f7c4464d1e6ff387be7bf7bde00fea16dd4
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-03-12 04:31:23 +0100

    task 17

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
-myVar = 333;
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
