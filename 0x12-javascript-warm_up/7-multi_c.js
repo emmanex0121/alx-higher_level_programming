2829769da66fb8c2a17a89ee08fe6542ba6eed45
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-03-12 03:12:25 +0100

    print x number of C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,11 +6,7 @@ const num = Number(process.argv[2]);
 if (arg === undefined) {
   console.log('Missing number of occurences');
 }
-/*
-if (num < 0){
-	console.log('');
-}
-*/
+
 for (let i = 0; i < num; i++) {
   console.log('C is fun');
 }
