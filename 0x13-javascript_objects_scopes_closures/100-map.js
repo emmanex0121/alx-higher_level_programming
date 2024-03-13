bbea8393cf08255363ab7585106fd2e85d1fa21a
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-03-13 18:00:48 +0100

    using map multiply array elem with index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
+const arr = require('./100-data').list;
 
-const newList = list.map((el, idx) => el * idx);
-console.log(list);
-console.log(newList);
+const newArr = arr.map((element, index) => element * index);
+console.log(arr);
+console.log(newArr);
