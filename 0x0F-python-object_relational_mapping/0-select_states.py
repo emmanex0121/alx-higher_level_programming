9cee3b2d323c6014c078d6f6ec8fe99a41d1327e
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-03-16 18:04:55 +0100

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
     data = cursor.fetchall()
 
