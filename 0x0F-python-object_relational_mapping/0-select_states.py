0fa27025d9002fe8bff72519403869c3f041df92
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-03-16 18:24:22 +0100

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,13 +5,13 @@
     Username Password and Database are provided as arguments respectively
 """
 
-import sys
+from sys import argv
 import MySQLdb
 
 if __name__ == '__main__':
-    MySQLdb.connect(user=sys.argv[1],
-                    passwd=sys.argv[2], db=sys.argv[3],
-                    host='localhost', port=3306)
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                         db=argv[3], host='localhost',
+                         port=3306)
 
     cursor = db.cursor()
 
